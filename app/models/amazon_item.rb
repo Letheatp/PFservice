50c63880085cfca71870bf0662f15666a337fc7f
Letheatp <letheatp@gmail.com>
2023-08-25 14:54:52 +0900

    バリデーションを設定

--- app/models/amazon_item.rb
+++ app/models/amazon_item.rb
@@ -1,3 +1,7 @@
 class AmazonItem < ApplicationRecord
   has_many :wishlist_items, foreign_key: "item_id", dependent: :destroy
+
+  validates :name, presence: true
+  validates :url, presence: true, uniqueness: true, format: /\A#{URI::regexp(%w(http https))}\z/
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,4 +1,6 @@
 class Wishlist < ApplicationRecord
   belongs_to :user
   has_many :wishlist_items, dependent: :destroy
+
+  validates :url, presence:true, uniqueness: true, format: /\A#{URI::regexp(%w(http https))}\z/
 end
