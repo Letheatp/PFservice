147d6ac3cb6df4c4ec93944062a2c25066add39d
Letheatp <letheatp@gmail.com>
2023-08-26 17:30:40 +0900

    feat: ヘッダーの作成とバリデーションエラーを表示

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,20 +1,21 @@
 class UserSessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_back_or_to(new_user_path, notice: 'ログインに成功しました')
+      redirect_back_or_to(root_path, success: 'ログインに成功しました')
     else
-      flash.now[:alert] = 'ログインに失敗しました'
-      render :new
+      flash[:danger] = 'ログインに失敗しました'
+      redirect_to login_path
     end
   end
 
   def destroy
     logout
-    redirect_to(login_path, notice: 'ログアウトしました')
+    redirect_to(login_path, success: 'ログアウトしました', status: :see_other)
   end
 end
