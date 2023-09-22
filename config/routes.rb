9a1725bd843ae9462da1fe7cd7227dcf3c60395a
Letheatp <letheatp@gmail.com>
2023-09-22 18:36:09 +0900

    設定画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  
   root "records#new"
-
+  get 'settings', to: 'settings#index'
   get 'login', to: 'user_sessions#new'
   post "login", to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
